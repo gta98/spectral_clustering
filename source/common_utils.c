bfee8b6e5c18f81b0815e90565a24a6e02dab349
gta98 <me@glub.xyz>
2022-07-25 11:26:37 +0300

    added argsort and removed redundant files

--- source/common_utils.c
+++ source/common_utils.c
@@ -25,3 +25,71 @@ int isanum(char* s) {
     }
     return 1;
 }
+
+/* argsort copied from: https://stackoverflow.com/questions/36714030/c-sort-float-array-while-keeping-track-of-indices */
+
+typedef struct {
+    real value;
+    int index;
+} validx_t;
+
+int cmp(const void *a, const void *b)
+{
+    validx_t* a1;
+    validx_t* a2;
+    a1 = (validx_t*) a;
+    a2 = (validx_t*) b;
+    if ((*a1).value > (*a2).value)
+        return -1;
+    else if ((*a1).value < (*a2).value)
+        return 1;
+    else
+        return 0;
+}
+
+uint* argsort(const real* v, const uint n) {
+    validx_t* indices;
+    uint* indices_uint;
+    uint i;
+    assert(v);
+
+    indices = NULL;
+    indices_uint = NULL;
+    indices = malloc(sizeof(validx_t)*n);
+    indices_uint = malloc(sizeof(uint)*n);
+    if (!indices || !indices_uint) {
+        if (indices) free(indices);
+        if (indices_uint) free(indices_uint);
+        return NULL;
+    }
+
+    for (i=0; i<n; i++) {
+        indices[i].index = i;
+        indices[i].value = v[i];
+    }
+
+    qsort(indices, n, sizeof(validx_t), cmp);
+    
+    for (i=0; i<n; i++) {
+        indices_uint[i] = indices[i].index;
+    }
+    return indices_uint;
+}
+
+uint* argsort_desc(const real* v, const uint n) {
+    uint i, max_i;
+    uint* indices;
+    indices = argsort(v, n);
+    if (!indices) return NULL;
+    for (i=0, max_i=floor(n/2); i<max_i; i++) {
+        swap(&(indices[i]), &(indices[n-i]));
+    }
+    return indices;
+}
+
+void swap(real* x, real* y) {
+    real z;
+    z = *x;
+    *x = *y;
+    *y = z;
+}
--- source/linalg.h
+++
@@ -1,8 +0,0 @@
-#ifndef H_LINALG
-#define H_LINALG
-
-#include "types.h"
-#include "matrix.h"
-
-
-#endif
