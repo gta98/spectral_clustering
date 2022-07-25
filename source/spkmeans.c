e55bdbd27d0e5bf923ec79a02a69da5440fc6ea2
gta98 <me@glub.xyz>
2022-07-25 12:41:56 +0300

    added calculation of k

--- source/spkmeans.c
+++ source/spkmeans.c
@@ -277,7 +277,7 @@ status_t sort_cols_by_vector_desc(mat_t* A, mat_t* v) {
     assert(A->h == A->w);
     assert(v->h == v->w);
     assert(A->h == v->h);
-    
+
     n = v->h;
     eigenvalues = malloc(sizeof(real)*n);
     if (!eigenvalues) return ERROR_MALLOC;
@@ -292,6 +292,34 @@ status_t sort_cols_by_vector_desc(mat_t* A, mat_t* v) {
     return SUCCESS;
 }
 
+bool is_diagonal(mat_t* A) {
+    return calc_off_squared(A) == 0;
+}
+
+bool is_square(mat_t* A) {
+    return A->w == A->h;
+}
+
+uint calc_k(mat_t* eigenvalues) {
+    uint i, n, half_n;
+    uint max_eigengap_idx;
+    real max_eigengap_val, eigengap_val;
+    assert(is_square(eigenvalues));
+    assert(is_diagonal(eigenvalues));
+    n = eigenvalues->h;
+    half_n = floor(((double)n)/2);
+    assert(half_n >= 1);
+    max_eigengap_idx = 0;
+    max_eigengap_val = mat_get(eigenvalues,0,0);
+    for (i=1; i<n; i++) {
+        eigengap_val = mat_get(eigenvalues,i,i);
+        if (eigengap_val > max_eigengap_val) {
+            max_eigengap_idx = i;
+            max_eigengap_val = eigengap_val;
+        }
+    }
+    return max_eigengap_idx;
+}
 
 
 mat_t* calc_P(mat_t* A) {
