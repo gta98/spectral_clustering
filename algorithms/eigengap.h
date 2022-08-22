85834cd50dd2df5dda43f63bd46061b960b86881
gta98 <me@glub.xyz>
2022-08-22 18:42:32 +0300

    missing header comment

--- algorithms/eigengap.h
+++ algorithms/eigengap.h
@@ -5,7 +5,10 @@
 #include "generics/common_utils.h"
 #include "generics/matrix.h"
 
+/* this sorts A and v by v, which can be either flat or square */
 status_t sort_cols_by_vector_desc(mat_t* A, mat_t* v);
+
+/* calculates k from eigenvalues according to eigengap heuristic */
 uint calc_k(mat_t* eigenvalues);
 
 #endif
